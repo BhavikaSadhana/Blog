af4219f9e0e08d52ade68a2da8002ff90e99f865
BhavikaSadhana <bhavikasadhana@gmail.com>
2025-03-25 22:46:29 +0530

    Add files via upload

---
+++ blog-server/src/main/java/com/bh/blog/dto/request/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.bh.blog.dto.request;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class LoginRequest {
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String password;
+}
